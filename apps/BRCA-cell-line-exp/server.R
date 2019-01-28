d06b7090ce5b8e882c52c1ed4a0e1192e0f9e524
leeoesterreich <43272587+leeoesterreich@users.noreply.github.com>
2019-01-28 14:22:46 -0500

    Add files via upload

--- apps/BRCA-cell-line-exp/server.R
+++ apps/BRCA-cell-line-exp/server.R
@@ -3,13 +3,13 @@ load("Neel_cell_line_subtype.rda")
 load("Cell_line_zGARP_Allgene.rda")
 shinyServer(function(input, output) {
   querydata <- reactive({
-    queryExpression <- RNAseq_normalized[rownames(RNAseq_normalized) %in% input$geneExpressionQuery,]
+    queryExpression <- Neel_cell_line_per_gene_normalized_log2_FPKM_Allgene[rownames(Neel_cell_line_per_gene_normalized_log2_FPKM_Allgene) %in% input$geneExpressionQuery,]
     queryExpression <- queryExpression[,-c(1,2,3)]
-    queryzGARP<-zGARP[rownames(zGARP) %in% input$geneExpressionQuery,]
+    queryzGARP<-Cell_line_zGARP_Allgene[rownames(Cell_line_zGARP_Allgene) %in% input$geneExpressionQuery,]
     queryzGARP<-queryzGARP[,-c(1,2,3)]
     queryzGARPdf<-t(queryzGARP)
     queryExpressiondf<-t(queryExpression)
-    querydata<-as.data.frame(matrix(NA,82,2))
+    querydata<-as.data.frame(matrix(NA,81,2))
     rownames(querydata)<-rownames(queryExpressiondf)
     colnames(querydata)<-c("Expression","zGARP")
     querydata[,1]<-queryExpressiondf[rownames(querydata),1]
@@ -33,8 +33,8 @@ shinyServer(function(input, output) {
     if(input$plotCommand == TRUE) {
       output$expressionPlot<-renderPlot({
         gene<-input$geneExpressionQuery
-        Genedata<-as.data.frame(t(data.matrix(RNAseq_normalized[gene,-c(1,2,3)])))
-        Genedata$histology<-cell_line_subtype[rownames(Genedata),"histology"]
+        Genedata<-as.data.frame(t(data.matrix(Neel_cell_line_per_gene_normalized_log2_FPKM_Allgene[gene,-c(1,2,3)])))
+        Genedata$histology<-Nell_cell_line_subtype[rownames(Genedata),"histology"]
         df<-as.data.frame(Genedata)
         colnames(df)<-c("gene", "histology")
         p<-wilcox.test(df[which(df$histology=="ER+ILC"),"gene"], df[which(df$histology=="Others"),"gene"], paired = F, alternative = "two.sided")$p.value
@@ -52,7 +52,7 @@ shinyServer(function(input, output) {
     if(input$barPlot == TRUE) {
       output$expressionPlot <- renderPlot({
         gene<-input$geneExpressionQuery
-        Genedata<-as.data.frame(t(data.matrix(RNAseq_normalized[gene,-c(1,2,3)])))
+        Genedata<-as.data.frame(t(data.matrix(Neel_cell_line_per_gene_normalized_log2_FPKM_Allgene[gene,-c(1,2,3)])))
         Genedata$expression<-2^Genedata[,1]
         Genedata_cell_line<-Genedata[c("MDAMB134","SUM44","MDAMB330","T47D","MCF7","BT474","MDAMB231","MCF10A","MCF12A"),]
         barplot(Genedata_cell_line$expression, main = "Normalized gene expression (FPKM)", col = "red",names =c("MM134","SUM44","MM330","T47D","MCF7","BT474","MM231","MCF10A","MCF12A"),cex.names = 1.2,las=2)
