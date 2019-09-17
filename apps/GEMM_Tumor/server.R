1039a9611db593485e96d8a1199b37ac2774c0d9
leeoesterreich <43272587+leeoesterreich@users.noreply.github.com>
2019-09-17 02:57:54 -0400

    Add files via upload

--- apps/GEMM_Tumor/server.R
+++ apps/GEMM_Tumor/server.R
@@ -1,9 +1,9 @@
 library(shiny)
 library(ggplot2)
-#load(PDX_counts)
 
+setwd("/Users/liz6/Box Sync/WCRC/Lee-Oesterreich-lab-personnel/Current/Vaciry/Vaciry/Bioinformatical analysis/shiny/GEMM_Tumor/")
 GEMM_CPM<-read.csv("GEMM_CPM.csv")
-group<-read.csv("Group.csv",stringsAsFactors = F)
+group<-read.csv("Group.csv",header=T)
 
 
 # Define server logic required to draw a histogram
@@ -13,13 +13,13 @@ shinyServer(function(input, output) {
  
  output$ER<- renderPlot({
    gene1<-input$gene
-   rownames(GEMM_CPM)=GEMM_CPM$X
+   rownames(GEMM_CPM)=make.unique(as.character(GEMM_CPM$Human))
    GEMM_CPM=GEMM_CPM[,2:18]
    GEMM_CPM1<-t(GEMM_CPM)
    class(GEMM_CPM1)
    GEMM_CPM1=as.data.frame(GEMM_CPM1)
    GEMM_CPM1$ER<-group$ER
-   df<-HCI_GEMM1[,c("ER",gene1)]
+   df<-GEMM_CPM1[,c("ER",gene1)]
    colnames(df)<-c("ER","gene1")
    
    p2<-wilcox.test(df[which(df$ER=="WT"),"gene1"], df[which(df$ER=="Mutant"),"gene1"], paired = F, alternative = "two.sided")$p.value
@@ -27,7 +27,7 @@ shinyServer(function(input, output) {
    
    boxplot(gene1 ~ ER, data=df,outpch=NA,ylab="TMMLog2(CPM+1)", par(cex.lab=1.5),par(cex.axis=1.5),par(cex.main=2),xlab=p2)
    stripchart(gene1 ~ ER, data = df, vertical = TRUE, method = "jitter",
-              pch = 20, col = c("red","blue","black"),add = TRUE) 
+              pch = 20, col = c("red","black"),add = TRUE) 
    title(paste(gene1, "Expression~ER Genotype"))
  })
 })
