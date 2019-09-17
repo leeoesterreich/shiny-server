7d7f99f5bc0334e758e76109247f58f1520a851a
leeoesterreich <43272587+leeoesterreich@users.noreply.github.com>
2019-09-17 02:30:34 -0400

    Add files via upload

---
+++ apps/GEMM_Tumor/server.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(ggplot2)
+#load(PDX_counts)
+
+GEMM_CPM<-read.csv("GEMM_CPM.csv")
+group<-read.csv("Group.csv",stringsAsFactors = F)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  output$Table<- renderTable(group)
+   
+ 
+ output$ER<- renderPlot({
+   gene1<-input$gene
+   rownames(GEMM_CPM)=GEMM_CPM$X
+   GEMM_CPM=GEMM_CPM[,2:18]
+   GEMM_CPM1<-t(GEMM_CPM)
+   class(GEMM_CPM1)
+   GEMM_CPM1=as.data.frame(GEMM_CPM1)
+   GEMM_CPM1$ER<-group$ER
+   df<-HCI_GEMM1[,c("ER",gene1)]
+   colnames(df)<-c("ER","gene1")
+   
+   p2<-wilcox.test(df[which(df$ER=="WT"),"gene1"], df[which(df$ER=="Mutant"),"gene1"], paired = F, alternative = "two.sided")$p.value
+   p2<-paste("Mann-Whitney U p-value (WT vs Mut)=", signif(p2, digits=2))
+   
+   boxplot(gene1 ~ ER, data=df,outpch=NA,ylab="TMMLog2(CPM+1)", par(cex.lab=1.5),par(cex.axis=1.5),par(cex.main=2),xlab=p2)
+   stripchart(gene1 ~ ER, data = df, vertical = TRUE, method = "jitter",
+              pch = 20, col = c("red","blue","black"),add = TRUE) 
+   title(paste(gene1, "Expression~ER Genotype"))
+ })
+})
+  
+
+
