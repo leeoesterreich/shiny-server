a7d1cd57ec41c66dad927f44326e42fb26534cb9
leeoesterreich <43272587+leeoesterreich@users.noreply.github.com>
2019-09-27 13:33:37 -0400

    Add files via upload

--- apps/GEMM_Tumor/server.R
+++ apps/GEMM_Tumor/server.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(ggplot2)
 #setwd("/Users/liz6/Box Sync/WCRC/Lee-Oesterreich-lab-personnel/Current/Vaciry/Vaciry/Bioinformatical analysis/Shiny/GEMM_Tumor/")
-load("GEMM_CPM3.Rda")
+GEMM_CPM1=read.csv("GEMM_CPM1.csv",header=T, row.names=1)
+GEMM_CPM3=read.csv("GEMM_CPM3.csv",header=T, row.names=1)
 group<-read.csv("Group.csv",header=T,stringsAsFactors = F)
 group2<-read.csv("Group2.csv",header=T,stringsAsFactors = F)
 
@@ -9,7 +10,21 @@ group2<-read.csv("Group2.csv",header=T,stringsAsFactors = F)
 shinyServer(function(input, output) {
   output$Table<- renderTable(group)
   
-  
+  output$ER<- renderPlot({
+    gene1<-input$gene
+    GEMM_CPM1$ER<-group$ER
+    df<-GEMM_CPM1[,c("ER",gene1)]
+    colnames(df)<-c("ER","gene1")
+    p<-wilcox.test(df[which(df$ER=="WT"),"gene1"], df[which(df$ER=="Mutant"),"gene1"], paired = F, alternative = "two.sided")$p.value
+    p<-paste("Mann-Whitney U test=", 
+             signif(p, digits=2))
+    
+    boxplot(gene1 ~ ER, data=df,outpch=NA,ylab="TMMlog2(CPM+1)",par(cex.lab=1.5),par(cex.axis=1.5), par(cex.main=2),xlab=p)
+    stripchart(gene1 ~ ER, data = df, vertical = TRUE, method = "jitter",
+               pch = 16, col = c("red","blue","black"),add = TRUE) 
+    
+    title(paste(gene1, "By Tumors"))
+  })
   output$ER2<- renderPlot({
     gene1<-input$gene
     GEMM_CPM3$ER<-group2$ER_by_mouse
