9edba9f7b5096c55292d19f194b1a099d417061a
leeoesterreich <43272587+leeoesterreich@users.noreply.github.com>
2019-11-23 18:56:19 -0500

    Add files via upload

---
+++ apps/E2_regulation/ui.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Gene Expression in ER+ Breast Cancer Cell Lines -/+ Estrogen"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      textInput("gene", label= "Enter Gene Symbol in CAPITAL letters", value = "ESR1"),
+
+      helpText("All data were extracted from the following resources:"),
+      strong("Disclaimer to users: Inter-data-sets normalization was not performmed. Please do not compare magnitude between data sets."),        
+      tableOutput("Table"),
+    width=3),
+    
+    mainPanel(
+      fluidRow(
+        column(3,plotOutput("MCF7_1")), 
+        column(3,plotOutput("MCF7_2")), 
+        column(3,plotOutput("MCF7_3")),
+        column(3,plotOutput("MCF7_4"))),
+      fluidRow(
+        column(3,plotOutput("T47D_1")),
+        column(3,plotOutput("T47D_2")), 
+        column(3,plotOutput("T47D_3")),
+        column(3,plotOutput("T47D_4"))),
+      fluidRow(
+        column(3,plotOutput("ZR")),
+        column(3,plotOutput("BT474")), 
+        column(3,plotOutput("MM134")),
+        column(3,plotOutput("SUM44")))
+    
+        
+      ))
+    
+  
+)
+)
