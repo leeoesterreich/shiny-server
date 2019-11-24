b48a999f5dbb97f6273d9948e6d919afba01df93
leeoesterreich <43272587+leeoesterreich@users.noreply.github.com>
2019-11-23 19:08:12 -0500

    Update ui.R

--- apps/E2_regulation/ui.R
+++ apps/E2_regulation/ui.R
@@ -9,7 +9,7 @@ shinyUI(fluidPage(
       textInput("gene", label= "Enter Gene Symbol in CAPITAL letters", value = "ESR1"),
 
       helpText("All data were extracted from the following resources:"),
-      strong("Disclaimer to users: Inter-data-sets normalization was not performed. Please do not compare magnitude between data sets."),        
+      strong("Disclaimer to users: Inter-data-sets normalization was not performed. Please do not compare magnitude between different data sets."),        
       tableOutput("Table"),
     width=3),
     
